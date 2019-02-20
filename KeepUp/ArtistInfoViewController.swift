6447ad5ade3606cd471ce6b693d2bfe7ded9cf3e
Prateek Kambadkone <pkambadkone@jhb.dvt.co.za>
2019-02-20 22:36:06 +0200

    *  prints serialised json response of artist searched

--- KeepUp/ArtistInfoViewController.swift
+++ KeepUp/ArtistInfoViewController.swift
@@ -12,12 +12,39 @@ import WebKit
 class ArtistInfoViewController: UIViewController {
 
     
-    
+    let apiString = "en.wikipedia.org/w/api.php?"
+    let apiReq = "action=query&prop=revisions&rvprop=content&format=jsonfm&rvsection=0"
     var artistName: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        artistName = artistName.replacingOccurrences(of: " ", with: "_")
+        let urlString = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&rvsection=0&titles=Linkin_Park"
+        
+        let url = URL(string: urlString)!
+        let session = URLSession.shared
+        let request = URLRequest(url: url)
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (data, response, error) in
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            
+            do{
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        
+        task.resume()
     }
 
     /*
