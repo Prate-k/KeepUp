dfd1e53c3c84fc512a09f2e4145c209a4fe75135
Prateek Kambadkone <pkambadkone@jhb.dvt.co.za>
2019-03-15 10:37:08 +0200

    * Folders for MVVM

---
+++ KeepUp/Login/Views/ViewControllers/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  KeepUp
+//
+//  Created by Prateek Kambadkone on 2019/03/15.
+//  Copyright © 2019 Prateek Kambadkone. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func LoginAction(_ sender: Any) {
+        //start progress bar on press and stop animation on viewWillDisappear
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let initialViewController = mainStoryboard.instantiateViewController(withIdentifier: "FirstScreen")
+        initialViewController.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+        self.present(initialViewController, animated: true, completion: nil)
+    }
+}
