fae9be102db1a588b6762d77a8a014c7bb96097d
Prateek Kambadkone <prate_k@yahoo.com>
2019-05-10 12:57:41 +0200

    * tab controller update

--- KeepUp/Common/Views/TabController.swift
+++ KeepUp/Common/Views/TabController.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
-class TabController: UITabBarController {
+class TabController: UITabBarController, UITabBarControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tabBarController?.delegate = self
         self.selectedIndex = 0
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        print("-------------------------------------------\n\n\n")
+        let index = tabBarController.selectedIndex
+        print("selected viewcontroller: \(viewController.title)")
+        if let navController = tabBarController.viewControllers?[tabBarController.selectedIndex] as? UINavigationController {
+            navController.popToRootViewController(animated: false)
+        } else {
+            viewController.navigationController?.popToRootViewController(animated: false)
+        }
+        self.selectedIndex = index
+        print("-------------------------------------------\n\n\n")
     }
 }
